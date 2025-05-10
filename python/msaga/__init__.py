aecdbcf401a8b7ebbc68f84e6fef59cfbecdb02e
Jiawei Lin <jiawei.lin@epfl.ch>
2025-05-10 22:18:47 +0200

    Added a simple memory allocator

--- python/msaga/__init__.py
+++ python/msaga/__init__.py
@@ -4,13 +4,13 @@ from .mem import CompoundMemoryManger
 from .tensor import STile, ATile, MTile
 from .dtype import *
 
-__mem_manger = CompoundMemoryManger()
+# TODO: do not hard code!
+__mem_manger = CompoundMemoryManger(
+    mem_base=0x80000000, mem_size=0x10000, mem_align=8,
+    spad_base=0, spad_size=0x1000, spad_align= 4 * fp16.itemsize, spad_dtype=fp16,
+    acc_base=0, acc_size=0x1000, acc_align= 4 * fp32.itemsize, acc_dtype=fp32
+)
 
-def alloc_spad() -> STile:
-    return __mem_manger.alloc_spad()
-
-def alloc_accumulator() -> ATile:
-    return __mem_manger.alloc_accumulator()
-
-def alloc_mem() -> MTile:
-    return __mem_manger.alloc_mem()
+alloc_spad = __mem_manger.alloc_spad
+alloc_accumulator = __mem_manger.alloc_accumulator
+alloc_mem = __mem_manger.alloc_mem
